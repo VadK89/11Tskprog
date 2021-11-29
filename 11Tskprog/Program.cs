b1a76d475e9675c325df905e689020dd2b2a4727
Вадим <Vadimk89@mail.ru>
2021-11-29 23:26:59 +0500

    Комментарии

--- 11Tskprog/Program.cs
+++ 11Tskprog/Program.cs
@@ -11,12 +11,20 @@ namespace _11Tskprog
     {
         static void Main(string[] args)
         {
+            /*1.	Разработать структуру для решения линейного уравнения 0=kx+b. 
+             * Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа. 
+             * Для решения уравнения предусмотреть метод Root. Создать экземпляр разработанной структуры. 
+             * Осуществить использование экземпляра в программе.
+             */
             Console.WriteLine("Введите значения коэффициентов уравнеия 0=k*x+b");
+            //Задаем структуру
             Solve sol = new Solve();
+            //Ввод исходных данных
             Console.WriteLine("Введите значения коэффициента k");
             sol.k = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Введите значения коэффициента b");
-            sol.b = Convert.ToDouble(Console.ReadLine());           
+            sol.b = Convert.ToDouble(Console.ReadLine());   
+            //Вывод результата
             sol.Root();
             Console.ReadKey();
         } 
