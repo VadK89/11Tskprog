c6157774291f1d3e6a1e0d334c6b71fa6dcb04d3
Вадим <Vadimk89@mail.ru>
2021-11-29 21:46:41 +0500

    Добавьте файлы проекта.

---
+++ 11Tskprog/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _11TskStruct;
+
+namespace _11Tskprog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значения коэффициентов уравнеия 0=k*x+b");
+            Solve sol = new Solve();
+            Console.WriteLine("Введите значения коэффициента k");
+            sol.k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значения коэффициента b");
+            sol.b = Convert.ToDouble(Console.ReadLine());           
+            sol.Root();
+            Console.ReadKey();
+        } 
+    
+    }
+}
